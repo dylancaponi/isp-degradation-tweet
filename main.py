8e0445381dcc031629fdc27c85d2953a037db678
Dylan Caponi <dc@MacBook-Pro-2.local>
2017-06-16 11:01:57 -0700

    add config_template

--- main.py
+++ main.py
@@ -10,29 +10,18 @@ logging.basicConfig(
     format="%(asctime)s:%(levelname)s:%(message)s"
     )
 
-with open('credentials.json') as f:    
-    creds = json.load(f)
-
-# the speed in Mbps that you are paying for 
-plan_speed = 300
-# your ISP's twitter handle
-isp_twitter_handle = '@Ask_Spectrum'
-# wifi or ethernet or anything you want to add to the message
-connection_type = 'wifi'
-# tweet will send under this percentage of degradation
-tweet_threshold = 80
-# number of tests to average
-# num_to_average = 3
+with open('config.json') as f:    
+    cfg = json.load(f)
 
 # constants
 bps_to_mbps = 1.0/1000000.0
 
-auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
-auth.set_access_token(creds['access_token'], creds['access_token_secret'])
+auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
 twitter_api = tweepy.API(auth)
 print twitter_api.me().name
 
-
+# servers = [1]
 s = speedtest.Speedtest()
 # s.get_servers(servers)
 s.get_best_server()
@@ -47,13 +36,14 @@ upload_speed = results_dict['upload'] * bps_to_mbps
 print results_dict
 logging.info(results_dict)
 
-percent_degraded = str(round(100*download_speed/plan_speed, 2))
-
-if percent_degraded < tweet_threshold:
-	
-	status = isp_twitter_handle + ' Service degraded to ' + percent_degraded + '% of ' + \
+plan_speed = cfg['plan_speed']
+percent_degraded = round(100*download_speed/plan_speed, 2)
+print percent_degraded
+print cfg['tweet_threshold']
+if percent_degraded < cfg['tweet_threshold']:
+	status = cfg['isp_twitter_handle'] + ' Service degraded to ' + str(percent_degraded) + '% of ' + \
 			str(plan_speed) + 'Mbps plan. ' + str(round(download_speed, 2)) + 'down ' + str(round(upload_speed, 2)) + 'up ' + \
-			str(results_dict['server']['latency']) + 'ping via ' + connection_type
+			str(results_dict['server']['latency']) + 'ping on ' + cfg['connection_type'] + ' via ' + results_dict['server']['sponsor']
 	print status
 	quit()
 	twitter_api.update_status(status=str(results_dict['download'] * bps_to_mbps))
