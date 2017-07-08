ffc5cb968c535dbf6609888765cefab1c7811462
Dylan Caponi <dc@MacBook-Pro-2.local>
2017-07-07 17:36:12 -0700

    format date

--- graph_results.py
+++ graph_results.py
@@ -1,7 +1,25 @@
 import json
+from datetime import datetime
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+from matplotlib import dates
 
 with open('results.log') as f:
-    results = [json.loads(line) for line in f]
+    df = pd.DataFrame(json.loads(line) for line in f)
 
-for line in results:
-	print line['download']
+print df.head()
+
+print df['download'].mean()
+
+df['timestamp'] = [datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ') for dt in df['timestamp']]
+
+axes = df.plot(x='timestamp', y='download', rot=15)
+# axes.xaxis.set_major_locator(dates.WeekdayLocator())
+# 2017-06-16T20:15:11.416971Z
+# print datetime.strptime(df.timestamp[0], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d %H:%M:%S')
+
+# quit()
+# axes.set_xticklabels([datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d') for dt in df.timestamp])
+plt.show()
